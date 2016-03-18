c6d6a0eaa8806e9acc8f49cf04260e2a0fef3284
Shogo Shimizu <shogoluc@gmail.com>
2016-03-18 09:36:25 -0400

    added content

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var $body = $('body');
+
+$('html').on('keydown', function (e) {
+  var $name = $('<div>');
+  var newLeft = Math.floor(Math.random() * (1000 - 0)) + 0;
+  var newTop = Math.floor(Math.random() * (1000 - 0)) + 0;
+
+  $name.addClass('ball');
+  $body.append($name);
+  $name.css('left', newLeft);
+  $name.css('top', newTop);
+})
